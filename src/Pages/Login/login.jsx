bd6e09ee749345856100d19fc5ba58359783aea5
Carlos <crodas035@gmail.com>
2021-10-04 18:25:52 +0200

    setting login form

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router';
 import styles from './login.module.scss';
 
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { login } from '../../Redux/actions/userActions/auth';
+
 const Login = () => {
-    
+
+
+    const [user, setUser] = useState({ email: '', password: '' });
+
+    const handleChange = (e) => {
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
+    const required = (value) => {
+        if (!value) {
+            return (
+                <div className="alert alert-danger" role="alert">
+                    This field is required!
+                </div>
+            );
+        }
+    };
+
+
+
     return (
         <div className={styles.container}>
-            <div className="form-leftside">
+            {/* <div className="form-leftside">
                 <img></img>
-            </div>
+            </div> */}
             <div className="form-rigthside">
-                <form>
+                <form onSubmit={handleSubmit()}>
                     <div className="form-item">
                         <label htmlFor="email">Email</label>
-                        <input type="text" id="email" />
+                        <input
+                            type="text"
+                            id="email"
+                            // onChange={(e) =>}
+                            // value={ }
+                        />
                     </div>
                     <div className="form-item">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input
+                            type="password"
+                            id="password"
+                            // onChange={}
+                            // value={ }
+                        />
                     </div>
                     <button type="submit" id="btn">Log in</button>
                 </form>
